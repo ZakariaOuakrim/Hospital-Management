decdf8b80855d293eae95d9124dee68cab29d365
ZakariaOuakrim <zakarialukyguy@gmail.com>
2025-03-18 11:09:57 +0000

    doctor entity added

---
+++ src/main/java/com/zakaria/hospitalmanagement/entities/Appointment.java
@@ -0,0 +1,9 @@
+package com.zakaria.hospitalmanagement.entities;
+
+import java.util.Date;
+
+public class Appointment {
+    private Long id;
+    private Date appointmentDate;
+    private Date appointmentHour;
+}
---
+++ src/main/java/com/zakaria/hospitalmanagement/entities/Doctor.java
@@ -0,0 +1,21 @@
+package com.zakaria.hospitalmanagement.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Doctor {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String speciality;
+    private String email;
+}
